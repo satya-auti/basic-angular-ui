488d15f6381037202a891d1d979a04384c975776
Satyawan Auti <auti.satyavan3@gmail.com>
2024-01-20 00:40:14 +0530

    fix: set route for signin and sign up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'authentication',
+        loadChildren: () =>
+          import('./authentication/authentication.module').then((e) => e.AuthenticationModule),
+      },
+      {
+        path: 'auth',
+        loadChildren: () =>
+          import('./layout/layout.module').then((e) => e.LayoutModule),
+      },
+      // {
+      //   path: 'customer',
+      //   loadChildren: () =>
+      //     import('./customer/customer.module').then((e) => e.CustomerModule),
+      // },
+      // {
+      //   // path: 'error',
+      //   // loadChildren: () => import('./error/error.module').then(e => e.ErrorModule)
+      // },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
