2f499199a1841ca681831442a7f8983b091f0b53
Satyawan Auti <auti.satyavan3@gmail.com>
2024-01-21 01:18:56 +0530

    fix: created components inside layout cards grid header footer sidebar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-rounting.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { GridComponent } from './grid/grid.component';
+import { CardsComponent } from './cards/cards.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidebarComponent,
+    FooterComponent,
+    GridComponent,
+    CardsComponent,
+    HeaderComponent,
+    
+  ],
   imports: [
     CommonModule,
     LayoutRoutingModule,
