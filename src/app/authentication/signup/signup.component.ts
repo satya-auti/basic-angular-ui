276c364ac0b65824c33b492ff056b90484b4f036
Satyawan Auti <auti.satyavan3@gmail.com>
2024-01-20 23:14:54 +0530

    fix: created sign up form

--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  public showPassword: boolean = false;
+  emailRegex: string = "";
+  error: string = ""
+  signUpForm: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+  
+  constructor() {
+    this.signUpForm = new FormGroup({
+      username: new FormControl('', [ Validators.email, Validators.pattern(this.emailRegex)]),
+      password: new FormControl('', [Validators.minLength(8)]),
+    });
+  }
+
+  submit(){
+    if (!this.signUpForm.valid) {
+      console.log("please enter valid data");
+      this.error = "Please enter correct username or password"
+      
+    }
+    if (this.signUpForm.valid) {
+      // this.submitEM.emit(this.form.value);
+      console.log("form data", this.signUpForm);
+      
+    }
+  }
+
+  public togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+  
 }
